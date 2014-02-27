991d73c0747c137fa20deb438266994ff37e1149
Sean McCleary <seanmcc@gmail.com>
2014-02-26 19:59:03 -0800

    initial bundle generated gem

---
+++ lib/activerecord/pg_array.rb
@@ -0,0 +1,7 @@
+require "activerecord/pg_array/version"
+
+module Activerecord
+  module PgArray
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/pg_array/version.rb
@@ -0,0 +1,5 @@
+module Activerecord
+  module PgArray
+    VERSION = "0.0.1"
+  end
+end
