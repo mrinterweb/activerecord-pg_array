340068f5c7a240434026875999b55de05e5d0c05
Sean McCleary <seanmcc@gmail.com>
2014-02-26 20:00:39 -0800

    added initial ActiveRecord::PGArray module
    First tests suggest that this is working

--- lib/activerecord/pg_array.rb
+++ lib/activerecord/pg_array.rb
@@ -1,7 +1,67 @@
 require "activerecord/pg_array/version"
 
-module Activerecord
-  module PgArray
-    # Your code goes here...
+module ActiveRecord
+  module PGArray
+
+    def self.included(base)
+      base.class_eval do
+        self.column_types.to_a.select { |c| c[1].instance_variable_get('@array') }.map(&:first).each do |array_column_name|
+          ids_regex = /_ids$/
+          friendly_attr = array_column_name.sub(ids_regex,'')
+          segs = friendly_attr.split('_')
+          segs[-1] = segs[-1].singularize
+          friendly_attr_singular = segs.join('_')
+          segs[-1] = segs[-1].pluralize
+          friendly_attr_plural = segs.join('_')
+
+          obj_convert = ->(obj) do
+            if array_column_name =~ ids_regex && obj.kind_of?(ActiveRecord::Base)
+              obj = obj.id
+            end
+            obj
+          end
+          atr = ->(slf) do
+            slf.send array_column_name.to_sym
+          end
+          atr_will_change = ->(slf) do
+            slf.send(:"#{array_column_name}_will_change!")
+          end
+
+          define_method :"add_#{friendly_attr_singular}" do |obj|
+            obj = obj_convert[obj]
+            unless atr[self].include?(obj)
+              atr[self].push(obj)
+              atr_will_change[self]
+            end
+          end
+
+          define_method :"add_#{friendly_attr_singular}!" do |obj|
+            self.send :"add_#{friendly_attr_singular}", obj
+            self.save!
+          end
+
+          define_method :"add_#{friendly_attr_plural}" do |objs|
+            objs.each do |obj|
+              self.send :"add_#{friendly_attr_singular}", obj
+            end
+          end
+
+          define_method :"remove_#{friendly_attr_singular}" do |obj|
+            obj = obj_convert.call(obj)
+            if atr[self].include?(obj)
+              atr[self].remove(obj)
+              atr_will_change[self]
+            end
+          end
+
+          define_method :"remove_#{friendly_attr_singular}!" do |obj|
+            self.send :"remove_#{friendly_attr_singular}", obj
+            self.save!
+          end
+
+        end
+      end # base.class_eval
+    end # self.include
+
   end
 end
